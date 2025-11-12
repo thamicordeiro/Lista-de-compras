db618688179207c8466cb7d92450a8918ec5bc2b
Thamires Cordeiro <thamirescordeiro1411@outlook.com>
2025-11-12 12:08:12 -0300

    Add o botão de remover na lista

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const addBtn = document.getElementById("add-btn");
 const list = document.getElementById("list");
 
 
-addBtn.addEventListener ("click", () => {
+addBtn.addEventListener("click", () => {
  const itemText = input.value.trim ();
 
  
@@ -12,6 +12,22 @@ if (itemText !== "") {
 const li = document.createElement("li");
 li.textContent = itemText;
 
+const removeBtn = document.createElement("button");
+removeBtn.textContent = "Remover";
+removeBtn.classList.add("remove-btn");
+
+
+li.addEventListener("click", () => {
+ li.classList.toggle("checked");
+});
+
+removeBtn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  li.remove();
+}
+)
+
+li.appendChild(removeBtn);
 
 list.appendChild(li);
 
